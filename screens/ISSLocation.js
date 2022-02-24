988696b13f40bd464f85ef1a25d8efe28187f296
SarveshHardas <86554291+SarveshHardas@users.noreply.github.com>
2022-02-24 21:15:40 +0530

    Add files via upload

---
+++ screens/ISSLocation.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class ISSLocation extends Component {
+ render(){
+   return (
+     <View>
+       <Text>Iss Location</Text>
+     </View>
+   )
+ }
+}
